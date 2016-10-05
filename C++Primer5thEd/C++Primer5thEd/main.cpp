101054d388ab0acdb5ea2b9039ebe257e9f7818f
Erik Martines Sanches <ernestms@kth.se>
2016-10-06 00:34:07 +0200

    First commit!

---
+++ C++Primer5thEd/C++Primer5thEd/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++Primer5thEd
+//
+//  Created by Erik Martines Sanches on 2016-10-06.
+//  Copyright © 2016 Erik Martines Sanches. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!!!\n";
+    return 0;
+}
