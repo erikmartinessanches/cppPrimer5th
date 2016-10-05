3fd1f1a028479e4ff48a73151abc918edeb81286
Erik Martines Sanches <ernestms@kth.se>
2016-10-06 01:34:33 +0200

    Exercise 1.9

---
+++ C++Primer5thEd/C++Primer5thEd/Ex1_9.cpp
@@ -0,0 +1,21 @@
+//
+//  Ex1_9.cpp
+//  C++Primer5thEd
+//
+//  Created by Erik Martines Sanches on 2016-10-06.
+//  Copyright © 2016 Erik Martines Sanches. All rights reserved.
+//
+
+#include <iostream>
+
+void exercise1_9(){
+    int sum = 0;
+    int val = 50;
+    while (val <= 100){
+        std::cout << "Summing " << sum << " and " << val <<"." << std::endl;
+        sum += val;
+        val++;
+    }
+    
+    std::cout << "Total sum: "<< sum << std::endl;
+}
--- C++Primer5thEd/C++Primer5thEd/main.cpp
+++ C++Primer5thEd/C++Primer5thEd/main.cpp
@@ -7,9 +7,15 @@
 //
 
 #include <iostream>
+#include "Ex1_9.cpp"
 
 int main(int argc, const char * argv[]) {
+    exercise1_9();
     // insert code here...
-    std::cout << "Hello, World!!!\n";
+//    std::cout << "Enter two numbers:" << std::endl;
+//    int v1 = 0, v2 = 0;
+//    std::cin >> v1 >> v2;
+//    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+    
     return 0;
 }
